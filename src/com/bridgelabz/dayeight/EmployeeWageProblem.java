28f042c4e053562fb07d2a6ad8afcac16f1d5ce4
sngtmth <sngtmth@gmail.com>
2021-10-02 19:14:33 +0530

    Added:Display Welcome Message

--- src/com/bridgelabz/dayeight/EmployeeWageProblem.java
+++ src/com/bridgelabz/dayeight/EmployeeWageProblem.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.dayeight;
-
+/**
+ * Employee Wage Computation Problem
+ * @author CHANDRU
+ *
+ */
 public class EmployeeWageProblem {
+	//Driver Code
 	public static void main(String args[]) {
 
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
