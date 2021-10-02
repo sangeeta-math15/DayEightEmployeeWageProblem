9f917699eea9d4e5e7f41a98ecb9dbf282666b7a
sngtmth <sngtmth@gmail.com>
2021-10-02 19:48:41 +0530

    Added:Checking Employee Present or Absent

--- src/com/bridgelabz/dayeight/EmployeeWageProblem.java
+++ src/com/bridgelabz/dayeight/EmployeeWageProblem.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.dayeight;
+
 /**
  * Employee Wage Computation Problem
+ * 
  * @author CHANDRU
  *
  */
 public class EmployeeWageProblem {
-	//Driver Code
+	// Driver Code
 	public static void main(String args[]) {
 
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		// use random function gives value 0 or 1
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		// print random values
+		System.out.println("Random values=>" + empCheck);
+		if (empCheck == 1) {
+			System.out.println("Employee Present");
+		} else {
+			System.out.println("Employee Absent");
+		}
+
 	}
 }
